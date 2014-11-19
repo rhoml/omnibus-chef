c6f097c52f0eb95cce61ffd29babe6868972b143
Serdar Sutay <serdar@opscode.com>
2014-11-19 11:14:18 -0800

    Testing audit-mode branch.

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -62,6 +62,8 @@ override :'ruby-windows', version: "2.0.0-p451"
 ######
 override :rubygems,       version: "2.4.1"
 
+override :chef,       version: "sersut/basic-audit-tests"
+
 dependency "preparation"
 dependency "chef"
 dependency "shebang-cleanup"
